cc9289e47309af4be567674e527e4dd7020db497
Everaldo Barreto Gomes Semedo <153240862+25-08-1984@users.noreply.github.com>
2024-08-28 21:50:50 -0300

    Add files via upload

---
+++ ProjetoAquivo/banco.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+class Banco():
+    def __init__(self):
+        self.conexao = sqlite3.connect('banco.db')
+        self.createTable()
+
+    def createTable(self):
+        c = self.conexao.cursor()
+        c.execute("""create table if not exists tbl_usuarios(
+        idusuario integer primary key autoincrement,
+        nome text,
+        telefone text,
+        email text,
+        usuario text,
+        senha text)""")
+
+        c =self.conexao.cursor()
+        c.execute("""create table if not exists tbl_cidades(
+        idcidade integer primary key autoincrement,
+        nomecid text,
+        uf       text)""")
+
+        c.execute("""create table if not exists tbl_clientes(
+        idcliente integer primary key autoincrement,
+        nomecli text,
+        
+        )""")
+
+        self.conexao.commit()
+        c.close()
